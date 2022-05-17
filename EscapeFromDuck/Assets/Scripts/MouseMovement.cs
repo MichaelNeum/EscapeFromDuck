621e517161aa831d4e624e501c56122006ec36c8
MichaelNeum <michael.neumayr@gmx.net>
2022-05-17 15:50:18 +0200

    adjusted mousemovement to work with controller

--- EscapeFromDuck/Assets/Scripts/MouseMovement.cs
+++ EscapeFromDuck/Assets/Scripts/MouseMovement.cs
@@ -1,31 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ControllerSpace;
 
 public class MouseMovement : MonoBehaviour
 {
     public float mouseSensitivity = 100.0f;
-    private float xRotation = 0;
+    private float yRotation = 0;
 
     public Transform Player;
-    // Start is called before the first frame update
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
         float xAxis = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity;
         float yAxis = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivity;
 
-        xRotation -= yAxis;
-        xRotation = Mathf.Clamp(xRotation, -90, 90);
+        yRotation -= yAxis - GameController.yAxis / 200;
+        yRotation = Mathf.Clamp(yRotation, -90, 90);
         if (GlobalData.PlayerData.alive)
         {
-            transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
-            Player.Rotate(Vector3.up * xAxis);
-        }   
+            transform.localRotation = Quaternion.Euler(yRotation, 0, 0);
+            Player.Rotate(Vector3.up * (xAxis - GameController.xAxis / 200));
+        }
     }
 }
